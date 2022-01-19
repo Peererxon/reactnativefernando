b15c818b8fc0689dae8b8ce7691128c91a35ce43
anderson gil <andersongil.dev@gmail.com>
2022-01-19 17:20:39 -0400

    flex propiedades

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
+import FlexScreen from './src/screens/FlexScreen'
 //import BoxObjectModelScreen from './src/screens/BoxObjectModelScreen'
 //import DimensionesScreen from './src/screens/DimensionesScreen'
-import PositionScreen from './src/screens/PositionScreen'
+//import PositionScreen from './src/screens/PositionScreen'
 
 export default function App() {
   return (
-      <PositionScreen />
+      <FlexScreen />
   )
 }
